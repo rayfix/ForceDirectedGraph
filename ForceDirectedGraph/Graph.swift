66052b25a61c013a593d1c67dd31b76eb5643055
Ray Fix <rayfix@gmail.com>
2019-07-20 14:49:51 -0700

    clean up the domain objects

--- ForceDirectedGraph/Graph.swift
+++ ForceDirectedGraph/Graph.swift
@@ -9,42 +9,44 @@
 import Foundation
 
 /// - Pure Model Objects
+
+/// A node represents a vertex of the graph (a dot)
 struct Node: Codable, Hashable {
-    var id: String
-    var group: Int
-    
-    init(id: String, group: Int) {
-        self.id = id
-        self.group = group
-    }
+  var id: String
+  var group: Int
 }
 
+/// A link is the edge between two nodes
 struct Link: Codable, Hashable {
-    var source: String
-    var target: String
-    var value: Int
+  var source: String
+  var target: String
+  var value: Int
 }
 
-final class Graph: Codable {
-    var nodes: [Node]
-    var links: [Link]
-    
-    init(jsonData: Data) throws {
-        let decoder = JSONDecoder()
-        let g = try decoder.decode(Self.self, from: jsonData)
-        (nodes, links) = (g.nodes, g.links)
+/// A graph is a collection of nodes and links between the nodes
+struct Graph: Codable {
+  var nodes: [Node]
+  var links: [Link]
+}
+
+/// Loading extensions of Graph that are part of the fundamental abstraction
+extension Graph {
+  enum Error: Swift.Error {
+    case fileNotFound(String)
+  }
+  
+  init(jsonData: Data) throws {
+    let decoder = JSONDecoder()
+    let decoded = try decoder.decode(Self.self, from: jsonData)
+    self.init(nodes: decoded.nodes, links: decoded.links)
+  }
+  
+  static func load(filename: String, bundle: Bundle = Bundle.main) throws -> Self {
+    guard let url = bundle.url(forResource: filename,
+                             withExtension: "json") else {
+                              throw Error.fileNotFound(filename)
     }
-    
-    enum Error: Swift.Error {
-        case fileNotFound(String)
-    }
-    
-    static func load(filename: String) throws -> Self {
-        guard let url = Bundle.main.url(forResource: filename,
-                                        withExtension: "json") else {
-            throw Error.fileNotFound(filename)
-        }
-        let data = try Data(contentsOf: url)
-        return try Self(jsonData: data)
-    }
+    let data = try Data(contentsOf: url)
+    return try Self(jsonData: data)
+  }
 }
